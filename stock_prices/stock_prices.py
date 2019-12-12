6e79869158104cc4fd1765d071a53e86156c6dff
Ben <allenben@protonmail.com>
2019-12-12 09:48:12 -0600

    MVP

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,15 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
-
+      sumOfSmallBig=0
+      for firstRound in prices:
+            for secondRound in prices[:prices.index(firstRound)]:
+                  if secondRound<firstRound:                                            
+                        if firstRound-secondRound>sumOfSmallBig:
+                              sumOfSmallBig=firstRound-secondRound             
+                  elif secondRound==prices[len(prices)-2] and sumOfSmallBig==0:
+                        sumOfSmallBig=firstRound-secondRound
+      return sumOfSmallBig
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -12,4 +19,4 @@ if __name__ == '__main__':
   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
   args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  print("A profit of ${profit} will be made of the prices: {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
